a1a832f2a4bc8faf793714483f1a0f216b6d0476
paytonwu90 <sw9008k@gmail.com>
2026-02-24 22:30:20 +0800

    feat: 完成路由設定

---
+++ src/views/front/SingleProduct.jsx
@@ -0,0 +1,9 @@
+function SingleProduct() {
+  return (
+    <div>
+      <h1>Single Product</h1>
+    </div>
+  );
+}
+
+export default SingleProduct;
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react2025-week5/' : '/',
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['color-functions', 'global-builtin', 'import', 'if-function'],
+      }
+    }
+  }
+})
