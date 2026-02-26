72eea7b2c08dd6f682b4db93e782cc9c22095bf9
paytonwu90 <sw9008k@gmail.com>
2026-02-27 03:59:51 +0800

    feat: 修改 Navbar 文字

--- src/layout/FrontendLayout.jsx
+++ src/layout/FrontendLayout.jsx
@@ -5,14 +5,14 @@ function FrontendLayout() {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">Home</NavLink>
+          <NavLink className="navbar-brand" to="/">首頁</NavLink>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link" to="/product">Products</NavLink>
+                <NavLink className="nav-link" to="/product">產品</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/cart">Cart</NavLink>
+                <NavLink className="nav-link" to="/cart">購物車</NavLink>
               </li>
             </ul>
           </div>
